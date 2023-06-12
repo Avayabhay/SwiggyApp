32ea2787cf22a140f9e7e38dcbe4e5b19d41f5fe
Abhay Sharma <avay.abhay@gmail.com>
2023-06-12 10:07:11 +0530

    restaurent Cards from real Data
    chap 5 completed.
    Added Restaurent Cards, understood props, key reconciliation, virtaul DOM. etx

--- src/App.js
+++ src/App.js
@@ -55,11 +55,11 @@ const Body = () => {
    */
   return (
     <div className="restaurent-list">
-      <RestaurentCard />
-      <RestaurentCard />
-      <RestaurentCard />
-      <RestaurentCard />
-      <RestaurentCard />
+      {restaurentList.map((restaurent) => {
+        return (
+          <RestaurentCard {...restaurent.data} key={restaurent.data?.id} />
+        );
+      })}
     </div>
   );
 };
@@ -76,26 +76,33 @@ const AppLayout = () => {
   );
 };
 
-const KFC = {
-  name: "KFC",
-  img: "https://b.zmtcdn.com/data/pictures/chains/7/20287/b9e1524a6ac4ffcf085cc410099833a9.jpg?output-format=webp&fit=around|771.75:416.25&crop=771.75:416.25;*,*",
-  cuisines: ["Burger", "Fast Food"],
-  rating: "4.5",
-};
+// const KFC = {
+//   name: "KFC",
+//   img: "https://b.zmtcdn.com/data/pictures/chains/7/20287/b9e1524a6ac4ffcf085cc410099833a9.jpg?output-format=webp&fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+//   cuisines: ["Burger", "Fast Food"],
+//   rating: "4.5",
+// };
 
-const RestaurentCard = () => {
+const RestaurentCard = ({
+  name,
+  cuisines,
+  cloudinaryImageId,
+  lastMileTravelString,
+}) => {
+  // const { name, cuisines } = props.restaurant;
+  // console.log(name);
   return (
     <div className="card">
       <img
         src={
           "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
-          restaurentList[0].data?.cloudinaryImageId
+          cloudinaryImageId
         }
         alt=" Logo"
       />
-      <h2>{restaurentList[0].data?.name}</h2>
-      <h3>{restaurentList[0].data?.cuisines.join(", ")}</h3>
-      <h4>{restaurentList[0].data?.lastMileTravelString} minutes away</h4>
+      <h2>{name}</h2>
+      <h3>{cuisines.join(", ")}</h3>
+      <h4>{lastMileTravelString} minutes away</h4>
     </div>
   );
 };
@@ -194,6 +201,120 @@ const restaurentList = [
       avgRating: "4.2",
       totalRatings: 10000,
       new: false,
+    },
+    subtype: "basic",
+  },
+  {
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "722436",
+      name: "Thirupathi Bhimas Fine Dine Restaurant",
+      uuid: "f047fb78-cd66-46aa-84e8-596ce22f01fc",
+      city: "1",
+      area: "Marathahalli",
+      totalRatingsString: "Too Few Ratings",
+      cloudinaryImageId: "a7c984c4681b9c53ec6caab27e36387a",
+      cuisines: [
+        "South Indian",
+        "Snacks",
+        "North Indian",
+        "Chinese",
+        "Continental",
+      ],
+      tags: [],
+      costForTwo: 30000,
+      costForTwoString: "₹300 FOR TWO",
+      deliveryTime: 40,
+      minDeliveryTime: 40,
+      maxDeliveryTime: 40,
+      slaString: "40 MINS",
+      lastMileTravel: 8.199999809265137,
+      slugs: {
+        restaurant:
+          "tirupati-bhimas-fine-dine-restaurant-marathahalli-marathahalli",
+        city: "bangalore",
+      },
+      cityState: "1",
+      address:
+        "Spice Garden, SRR Arcade, Bus Stop, 35/1B, Varthur Rd, Lakshminarayana Pura, Marathahalli, Bengaluru, Karnataka 560037",
+      locality: "Munnekollala",
+      parentId: 430180,
+      unserviceable: false,
+      veg: true,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      ribbon: [
+        {
+          type: "PROMOTED",
+        },
+      ],
+      chain: [],
+      feeDetails: {
+        fees: [
+          {
+            name: "distance",
+            fee: 6700,
+            message: "",
+          },
+          {
+            name: "time",
+            fee: 0,
+            message: "",
+          },
+          {
+            name: "special",
+            fee: 0,
+            message: "",
+          },
+        ],
+        totalFees: 6700,
+        message: "",
+        title: "Delivery Charge",
+        amount: "6700",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "cid=7084865~p=16~eid=00000188-a847-ad04-1098-7f8e00021056",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "8.1 kms",
+      hasSurge: false,
+      aggregatedDiscountInfoV3: {
+        header: "25% OFF",
+        subHeader: "ABOVE ₹600",
+        discountTag: "FLAT DEAL",
+        headerTypeV2: 0,
+      },
+      sla: {
+        restaurantId: "722436",
+        deliveryTime: 40,
+        minDeliveryTime: 40,
+        maxDeliveryTime: 40,
+        lastMileTravel: 8.199999809265137,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: true,
+      avgRating: "--",
+      totalRatings: 0,
+      new: true,
     },
     subtype: "basic",
   },
@@ -512,6 +633,102 @@ const restaurentList = [
     type: "restaurant",
     data: {
       type: "F",
+      id: "31192",
+      name: "Sri Udupi Park - Opp Bescom",
+      uuid: "e43a475d-3304-469d-9b7f-689eca6980ca",
+      city: "1",
+      area: "Mahadevapura",
+      totalRatingsString: "10000+ ratings",
+      cloudinaryImageId: "qhiy6coolxjequzdstn0",
+      cuisines: ["South Indian", "North Indian", "Jain", "Chinese", "Desserts"],
+      tags: [],
+      costForTwo: 20000,
+      costForTwoString: "₹200 FOR TWO",
+      deliveryTime: 33,
+      minDeliveryTime: 33,
+      maxDeliveryTime: 33,
+      slaString: "33 MINS",
+      lastMileTravel: 6.300000190734863,
+      slugs: {
+        restaurant: "sri-udupi-park-mahadevpura-mahadevpura",
+        city: "bangalore",
+      },
+      cityState: "1",
+      address:
+        "Opp BESCOM office,near Phoenix mall, ITPL road ,Mahadevapura-48",
+      locality: "Phoenix Marketcity Mall",
+      parentId: 381856,
+      unserviceable: false,
+      veg: true,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      chain: [],
+      feeDetails: {
+        fees: [
+          {
+            name: "distance",
+            fee: 5100,
+            message: "",
+          },
+          {
+            name: "time",
+            fee: 0,
+            message: "",
+          },
+          {
+            name: "special",
+            fee: 0,
+            message: "",
+          },
+        ],
+        totalFees: 5100,
+        message: "",
+        title: "Delivery Charge",
+        amount: "5100",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "6.3 kms",
+      hasSurge: false,
+      sla: {
+        restaurantId: "31192",
+        deliveryTime: 33,
+        minDeliveryTime: 33,
+        maxDeliveryTime: 33,
+        lastMileTravel: 6.300000190734863,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: false,
+      avgRating: "4.2",
+      totalRatings: 10000,
+      new: false,
+    },
+    subtype: "basic",
+  },
+  {
+    type: "restaurant",
+    data: {
+      type: "F",
       id: "362437",
       name: "UDUPI VEG",
       uuid: "f303a0c9-cdcc-4061-b87a-cc9896837f32",
@@ -606,6 +823,222 @@ const restaurentList = [
       promoted: false,
       avgRating: "4.0",
       totalRatings: 500,
+      new: false,
+    },
+    subtype: "basic",
+  },
+  {
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "722436",
+      name: "Thirupathi Bhimas Fine Dine Restaurant",
+      uuid: "f047fb78-cd66-46aa-84e8-596ce22f01fc",
+      city: "1",
+      area: "Marathahalli",
+      totalRatingsString: "Too Few Ratings",
+      cloudinaryImageId: "a7c984c4681b9c53ec6caab27e36387a",
+      cuisines: [
+        "South Indian",
+        "Snacks",
+        "North Indian",
+        "Chinese",
+        "Continental",
+      ],
+      tags: [],
+      costForTwo: 30000,
+      costForTwoString: "₹300 FOR TWO",
+      deliveryTime: 40,
+      minDeliveryTime: 40,
+      maxDeliveryTime: 40,
+      slaString: "40 MINS",
+      lastMileTravel: 8.199999809265137,
+      slugs: {
+        restaurant:
+          "tirupati-bhimas-fine-dine-restaurant-marathahalli-marathahalli",
+        city: "bangalore",
+      },
+      cityState: "1",
+      address:
+        "Spice Garden, SRR Arcade, Bus Stop, 35/1B, Varthur Rd, Lakshminarayana Pura, Marathahalli, Bengaluru, Karnataka 560037",
+      locality: "Munnekollala",
+      parentId: 430180,
+      unserviceable: false,
+      veg: true,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      ribbon: [
+        {
+          type: "PROMOTED",
+        },
+      ],
+      chain: [],
+      feeDetails: {
+        fees: [
+          {
+            name: "distance",
+            fee: 6700,
+            message: "",
+          },
+          {
+            name: "time",
+            fee: 0,
+            message: "",
+          },
+          {
+            name: "special",
+            fee: 0,
+            message: "",
+          },
+        ],
+        totalFees: 6700,
+        message: "",
+        title: "Delivery Charge",
+        amount: "6700",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "cid=7084865~p=16~eid=00000188-a847-ad04-1098-7f8e00021056",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "8.1 kms",
+      hasSurge: false,
+      aggregatedDiscountInfoV3: {
+        header: "25% OFF",
+        subHeader: "ABOVE ₹600",
+        discountTag: "FLAT DEAL",
+        headerTypeV2: 0,
+      },
+      sla: {
+        restaurantId: "722436",
+        deliveryTime: 40,
+        minDeliveryTime: 40,
+        maxDeliveryTime: 40,
+        lastMileTravel: 8.199999809265137,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: true,
+      avgRating: "--",
+      totalRatings: 0,
+      new: true,
+    },
+    subtype: "basic",
+  },
+
+  {
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "548654",
+      name: "Sandwich 2 Poha",
+      uuid: "dd3c7800-3cf3-46fa-86d2-c11327bedbf5",
+      city: "1",
+      area: "Marathahalli",
+      totalRatingsString: "1000+ ratings",
+      cloudinaryImageId: "uy9hnrrhljvqzjtnrar4",
+      cuisines: ["Healthy Food", "Indian", "Continental"],
+      tags: [],
+      costForTwo: 25000,
+      costForTwoString: "₹250 FOR TWO",
+      deliveryTime: 34,
+      minDeliveryTime: 34,
+      maxDeliveryTime: 34,
+      slaString: "34 MINS",
+      lastMileTravel: 7.599999904632568,
+      slugs: {
+        restaurant: "sandwich-2-poha-marathahalli-marathahalli",
+        city: "bangalore",
+      },
+      cityState: "1",
+      address:
+        "1566, 3rd Main Rd, E Block, AECS Layout, Brookefield, Bengaluru, Karnataka 560037, India",
+      locality: "AECS Layout",
+      parentId: 329823,
+      unserviceable: false,
+      veg: false,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      ribbon: [
+        {
+          type: "PROMOTED",
+        },
+      ],
+      chain: [],
+      feeDetails: {
+        fees: [
+          {
+            name: "distance",
+            fee: 5700,
+            message: "",
+          },
+          {
+            name: "time",
+            fee: 0,
+            message: "",
+          },
+          {
+            name: "special",
+            fee: 0,
+            message: "",
+          },
+        ],
+        totalFees: 5700,
+        message: "",
+        title: "Delivery Charge",
+        amount: "5700",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "cid=7094454~p=13~eid=00000188-a847-ad04-1098-7f8d00020d28",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "7.5 kms",
+      hasSurge: false,
+      sla: {
+        restaurantId: "548654",
+        deliveryTime: 34,
+        minDeliveryTime: 34,
+        maxDeliveryTime: 34,
+        lastMileTravel: 7.599999904632568,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: true,
+      avgRating: "4.2",
+      totalRatings: 1000,
       new: false,
     },
     subtype: "basic",

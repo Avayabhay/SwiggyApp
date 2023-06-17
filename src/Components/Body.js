5ac396d6205c020a17c8c70dea8cc3908d6be806
Abhay Sharma <avay.abhay@gmail.com>
2023-06-17 17:33:16 +0530

    completed - Search functionality

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,29 +1,48 @@
+import { useState } from "react";
 import { restaurentList } from "../constants";
 import RestaurentCard from "./RestaurentCard";
 
 const Body = () => {
-  /**
-   * SearchBAr
-   * Restaurent List
-   *  -Restaurent Cards(many)
-   *    - Image
-   *    - Name
-   *    - Cousine
-   *    -  Rating
-   *
-   *
-   */
+  const [searchText, setSearchText] = useState("");
+  const [restaurents, setRestaurents] = useState(restaurentList);
+
+  const filterRestaurents = (text) => {
+    return restaurentList.filter((restaurent) =>
+      restaurent.data.name.toLowerCase().includes(text)
+    );
+  };
+
   return (
-    <div className="restaurent-list">
-      {restaurentList.map((restaurent, index) => {
-        return (
-          <RestaurentCard
-            {...restaurent.data}
-            key={restaurent.data?.id + index}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search Restaurent"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="search-btn"
+          onClick={() => {
+            const data = filterRestaurents(searchText.toLowerCase());
+            setRestaurents(data);
+          }}
+        >
+          Search
+        </button>{" "}
+      </div>
+      <div className="restaurent-list">
+        {restaurents.map((restaurent, index) => {
+          return (
+            <RestaurentCard
+              {...restaurent.data}
+              key={restaurent.data?.id + index}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 };
 

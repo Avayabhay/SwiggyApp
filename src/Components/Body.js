7dce57572c52174639d293f4a3f1d4b9131bd8a7
Abhay Sharma <avay.abhay@gmail.com>
2023-06-20 08:16:35 +0530

    useEffect Hook & implemented real API for restaurents

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,15 +1,30 @@
-import { useState } from "react";
-import { restaurentList } from "../constants";
+import { useEffect, useState } from "react";
+//import { restaurentList } from "../constants";
 import RestaurentCard from "./RestaurentCard";
 
 const Body = () => {
   const [searchText, setSearchText] = useState("");
-  const [restaurents, setRestaurents] = useState(restaurentList);
+  const [restaurents, setRestaurents] = useState([]);
 
   const filterRestaurents = (text) => {
-    return restaurentList.filter((restaurent) =>
+    return restaurents.filter((restaurent) =>
       restaurent.data.name.toLowerCase().includes(text)
     );
+  };
+
+  //Use Effects
+  useEffect(() => {
+    // Get the data from the API
+    getRestaurenets();
+  }, []);
+
+  const getRestaurenets = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.984048&lng=77.7481552&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setRestaurents(json.data.cards[2].data.data.cards);
+    console.log(json);
   };
 
   return (

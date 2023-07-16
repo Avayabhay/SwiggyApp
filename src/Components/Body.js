678f6dbec1dd4e46c3fe51c373b8ed3cb2a32be9
Abhay Sharma <avay.abhay@gmail.com>
2023-07-16 11:49:12 +0530

    moved API call logic for all restaurents to useGetAllRestaurents hook

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,30 +1,16 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 //import { restaurentList } from "../constants";
 import RestaurentCard from "./RestaurentCard";
 import ShimmerUI from "./ShimmerUI";
 import { Link } from "react-router-dom";
 import { filterRestaurents } from "../Utils/helper";
+import useGetAllRestaurents from "../Utils/useGetAllRestaurents";
 
 const Body = () => {
   const [searchText, setSearchText] = useState("");
-  const [filteredrestaurents, setFilteredRestaurents] = useState([]);
-  const [allRestaurents, setAllRestaurents] = useState([]);
 
-  //Use Effects
-  useEffect(() => {
-    // Get the data from the API
-    getRestaurenets();
-  }, []);
-
-  const getRestaurenets = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.984048&lng=77.7481552&page_type=DESKTOP_WEB_LISTING"
-    );
-    const json = await data.json();
-    setFilteredRestaurents(json.data.cards[2].data.data.cards);
-    setAllRestaurents(json.data.cards[2].data.data.cards);
-    console.log(json);
-  };
+  const [allRestaurents, filteredrestaurents, setFilteredRestaurents] =
+    useGetAllRestaurents();
 
   return allRestaurents.length === 0 ? (
     <ShimmerUI />

9bf322787f35b0a7688f66b40f68441b5b02e532
Abhay Sharma <avay.abhay@gmail.com>
2023-08-05 18:05:36 +0530

    made Accordion - the good way

--- src/Components/Instamart.js
+++ src/Components/Instamart.js
@@ -8,7 +8,11 @@ const Section = ({ title, description, isVisible, setIsVisible }) => {
       <h2 className="text-xl ">{title}</h2>
       <button
         className="bg-blue-300 rounded-sm px-2 my-2 border-2 border-blue-900 hover:bg-green-300 "
-        onClick={() => setIsVisible(!isVisible)}
+        onClick={() => {
+          if (isVisible) {
+          }
+          setIsVisible(!isVisible);
+        }}
       >
         {isVisible ? <p>Hide</p> : <p>Show</p>}
       </button>
@@ -18,11 +22,7 @@ const Section = ({ title, description, isVisible, setIsVisible }) => {
 };
 
 const Instamart = () => {
-  const [sectionConfig, setSectionConfig] = useState({
-    showAbout: false,
-    showCareers: false,
-    showNearByMarts: false,
-  });
+  const [sectionConfig, setSectionConfig] = useState("about");
 
   return (
     <>
@@ -37,42 +37,24 @@ const Instamart = () => {
         description={
           "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
         }
-        isVisible={sectionConfig.showCareers}
-        setIsVisible={() =>
-          setSectionConfig({
-            showAbout: false,
-            showCareers: true,
-            showNearByMarts: false,
-          })
-        }
+        isVisible={sectionConfig === "careers"}
+        setIsVisible={() => setSectionConfig("careers")}
       />
       <Section
         title={"About"}
         description={
           "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
         }
-        isVisible={sectionConfig.showAbout}
-        setIsVisible={() =>
-          setSectionConfig({
-            showAbout: true,
-            showCareers: false,
-            showNearByMarts: false,
-          })
-        }
+        isVisible={sectionConfig === "about"}
+        setIsVisible={() => setSectionConfig("about")}
       />
       <Section
         title={"Near-by Marts"}
         description={
           "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
         }
-        isVisible={sectionConfig.showNearByMarts}
-        setIsVisible={() =>
-          setSectionConfig({
-            showAbout: false,
-            showCareers: false,
-            showNearByMarts: true,
-          })
-        }
+        isVisible={sectionConfig === "nearByMarts"}
+        setIsVisible={() => setSectionConfig("nearByMarts")}
       />
     </>
   );

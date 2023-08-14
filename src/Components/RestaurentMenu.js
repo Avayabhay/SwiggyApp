cb4d7ed078459984bb88d0e79d8adb98ea955fa9
Abhay Sharma <avay.abhay@gmail.com>
2023-08-14 16:45:49 +0530

    Dispatching action to add items to Cart

--- src/Components/RestaurentMenu.js
+++ src/Components/RestaurentMenu.js
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom";
 import { IMG_CDN_URL } from "../constants";
 import ShimmerUI from "./ShimmerUI";
 import useRestaurent from "../Utils/useRestaurent";
+import { useDispatch } from "react-redux";
+import { addItem } from "../Utils/cartSlice";
 
 const RestaurentMenu = () => {
   const { id } = useParams();
@@ -9,6 +11,12 @@ const RestaurentMenu = () => {
 
   const [restaurent, menuItems] = useRestaurent(id);
 
+  const handleAddItem = () => {
+    dispatch(addItem("Grapes"));
+  };
+
+  //Using Dispatch
+  const dispatch = useDispatch();
   return !restaurent ? (
     <ShimmerUI />
   ) : (
@@ -24,6 +32,14 @@ const RestaurentMenu = () => {
         <h3>{restaurent.costForTwoMessage}</h3>
         <h3>{restaurent.avgRating} Stars</h3>
       </div>
+
+      <button
+        className="p-2 m-4 bg-blue-400 rounded-md"
+        onClick={() => handleAddItem()}
+      >
+        Add Grapes
+      </button>
+
       <div className="restaurent-menu-items">
         <h2>MENU</h2>
         <ul>
